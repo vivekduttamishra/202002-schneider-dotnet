29132faa6e4b773281850395858a6d4e2bd18fcc
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-02-10 16:28:44 +0530

    day#1-update#4

---
+++ cs-projects/TriangleDemos/Demo01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle(3,4,5);
+            //t1.Set(3, 4, 5);  // it is verified valid
+            TestTriangle(t1);
+
+            Triangle t2 = new Triangle(3,6,12);
+            //t2.Set(3, 6, 12);
+            TestTriangle(t2);
+
+            //t1.s3 = 100;     //now its invalid, but Triangle doesn't know it has become invalid
+            t1.Set(3, 4, 100);  //now system knows this new combination is invalid
+            
+            TestTriangle(t1);
+
+            Triangle t3 = new Triangle();
+            //sides are not set?
+            TestTriangle(t3);
+
+        }
+        private static void TestTriangle(Triangle t1)
+        {
+            t1.Draw();
+            Console.WriteLine("Perimeter is " + t1.Perimeter());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ cs-projects/TriangleDemos/Demo01/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    class Triangle
+    {
+        //private unless marked otherwise
+        int s1, s2, s3;
+        bool valid = true;
+
+        public Triangle()
+        {
+            Set(1, 1, 1);
+        }
+
+        public Triangle(int s1, int s2, int s3)
+        {
+            Set(s1, s2, s3);
+        }
+
+        public void Set(int s1,int s2,int s3)
+        {
+            if(s1>0 && s2>0 && s3>0 && s1+s2>s3 && s1+s3>s2 && s2 + s3 > s1)
+            {
+                this.s1 = s1;
+                this.s2 = s2;
+                this.s3 = s3;
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+
+        public bool IsValid()
+        {
+            return valid;
+        }
+
+        public int Perimeter()
+        {
+            if (IsValid())
+                return s1 + s2 + s3;
+            else
+                return -1; //indicates error
+        }
+
+        public void Draw()
+        {
+            if(IsValid())
+                Console.WriteLine("Triangle <{0},{1},{2}> drawn",s1,s2,s3);
+            else
+                Console.WriteLine("Invalid Triangle");
+        }
+    }
+}
